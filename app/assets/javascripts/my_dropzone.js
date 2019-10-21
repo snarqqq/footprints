f91e2e75296db7ef26cda49dda10d35e05d5fbc7
snarqqq <yttkym2357@gmail.com>
2019-10-21 16:33:05 +0900

    conditioned dropzone instantiate

--- app/assets/javascripts/my_dropzone.js
+++ app/assets/javascripts/my_dropzone.js
@@ -1,29 +1,34 @@
 let myDropzone;
 
 $(function(){
-  Dropzone.autoDiscover = false;
+  console.log(window.location.href);
+  if ($("#file-drop-area")[0] !== undefined){
+  console.log('working');
+    Dropzone.autoDiscover = false;
     myDropzone = new Dropzone("#file-drop-area", {
-    url: "/posts",
-    uploadMultiple: true,
-    parallelUploads: 10,
-    paramName: "images[image]",
-    maxFiles: 10,
-    previewsContainer: '.dropzone-previews',
-    addRemoveLinks: true,
-    dictRemoveFile: "削除",
-    dictMaxFilesExceeded: "10 files limit",
-    dictInvalidFileType: "Image file only",
-    autoProcessQueue: false
-  });
+      url: "/posts",
+      uploadMultiple: true,
+      parallelUploads: 10,
+      paramName: "images[image]",
+      maxFiles: 10,
+      previewsContainer: '.dropzone-previews',
+      addRemoveLinks: true,
+      dictRemoveFile: "削除",
+      dictMaxFilesExceeded: "10 files limit",
+      dictInvalidFileType: "Image file only",
+      autoProcessQueue: false
+    });
 
-  // 写真アップロード説明文の表示切り替え
-  myDropzone.on('addedfile', function(file) {
-    $('#newpost-modal__image-text').css('display', 'none');
-  });
-  myDropzone.on('removedfile', function(file) {
-    if ($('.dropzone-previews').children().length === 0) {
-      $('#newpost-modal__image-text').css('display', 'block');
-    }
-  });
-  
+    // 写真アップロード説明文の表示切り替え
+    myDropzone.on('addedfile', function(file) {
+      $('#newpost-modal__image-text').css('display', 'none');
+    });
+    myDropzone.on('removedfile', function(file) {
+      if ($('.dropzone-previews').children().length === 0) {
+        $('#newpost-modal__image-text').css('display', 'block');
+      }
+    });
+
+  };
+
 });
