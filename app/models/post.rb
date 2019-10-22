1e388fdd75519a59f903bb3f2a10ce1d7b22c310
snarqqq <yttkym2357@gmail.com>
2019-10-22 11:14:09 +0900

    post title max-length validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,7 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :images, dependent: :destroy
   # accepts_nested_attributes_for :images
+
+  validates :title, length: { maximum: 40 }
   # validates :body, presence: true
 end
