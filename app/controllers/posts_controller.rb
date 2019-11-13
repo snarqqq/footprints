01ec1e91b33ad32b881ada74f736e4a4bfcbb8a0
snarqqq <yttkym2357@gmail.com>
2019-11-13 15:40:13 +0900

    show latest posts on root page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
   # GET /posts.json
   # 一つのリクエストに対して複数のフォーマットで返せない？(htmlとjson)
   def index
-    @posts = current_user.posts.order("created_at DESC").includes(:place, :images)
+    @posts = Post.where("id != ?", current_user.id).limit(21)
+    @my_recent_posts = current_user.posts.order("created_at DESC").includes(:place, :images)
   end
 
   # GET /posts/1
