89df136baec7409f21fd7ba74e56045156c0da9d
snarqqq <yttkym2357@gmail.com>
2019-11-14 23:02:45 +0900

    def user_icon function in application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def user_icon(user)
+    if user.avatar?
+      image_tag user.avatar_url, class: "my-navbar__avatar"
+    elsif user.image?
+      image_tag user.image, class: "my-navbar__avatar"
+    else
+      fa_icon "user-circle", class: "fa-2x"
+    end
+  end
 end
