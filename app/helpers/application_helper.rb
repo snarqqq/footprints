63f9e78e0e6c97fb066280dcbb871eb16e0551eb
snarqqq <yttkym2357@gmail.com>
2019-11-15 16:09:21 +0900

    add class_name arg to user_icon function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def user_icon(user)
+  def user_icon(user, class_name)
     if user.avatar?
-      image_tag user.avatar_url, class: "my-navbar__avatar"
+      image_tag user.avatar_url, class: class_name
     elsif user.image?
-      image_tag user.image, class: "my-navbar__avatar"
+      image_tag user.image, class: class_name
     else
-      fa_icon "user-circle", class: "fa-2x"
+      fa_icon "user-circle", class: "fa-2x #{class_name}"
     end
   end
 end
