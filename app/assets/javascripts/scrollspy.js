0fcb1fdef4a4ef113b8125e30ff33b85e7517d7c
snarqqq <yttkym2357@gmail.com>
2019-11-22 21:05:58 +0900

    implemented scrollspy

---
+++ app/assets/javascripts/scrollspy.js
@@ -0,0 +1,21 @@
+$(document).on('turbolinks:load', function() {
+  // Init Scrollspy
+  $('body').scrollspy({ target: '#main-nav' });
+
+  // Smooth Scrolling
+  $("#main-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        window.location.hash = hash;
+      });
+    }
+  });
+
+});
