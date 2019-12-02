3f1fc33064ac1fb437b4ec87de337c54a7427d2c
snarqqq <yttkym2357@gmail.com>
2019-12-02 11:00:41 +0900

    excluded others' wannagos marker

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::PostsController < ApplicationController
   def index
     @my_recent_posts = current_user.posts.order(created_at: :asc).includes(:place, :images)
-    @posts = Post.where("user_id != ?", current_user.id).order(created_at: :desc).limit(21).includes(:place, :images, :user)
+    @posts = Post.where('user_id != ? and already_visited = ?', current_user.id, 1).order(created_at: :desc).limit(21).includes(:place, :images, :user)
   end
 end
